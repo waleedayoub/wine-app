a3e25283ef393e5333577ce4c00a9f4c20d02fef
selvam palanimalai <selvam.palanimalai@gmail.com>
2022-09-10 20:56:57 -0400

    adding signup page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -21,6 +21,7 @@ const Header: React.FC = () => {
       </>
     } else {
     return <>
+      <a className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" href="/signup">Sign Up</a> 
       <a className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={() => signIn()}>Sign in</a>
     </>
     }
